e72a156abf19f1bdce3e02784376f749c2fe1ee3
alan-acosta2410 <alansebasacosta2410@gmail.com>
2025-08-17 03:03:51 -0300

    Evento con div

--- eventos.js
+++ eventos.js
@@ -1,2 +1,12 @@
 function saludar() {
     alert("Hola!") }
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const div = document.getElementById("div1");
+
+  div.addEventListener("click", () => {
+    alert("Hola! Soy el div");
+  });
+});
